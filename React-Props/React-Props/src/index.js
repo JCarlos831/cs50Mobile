087bbb70647a881e6ac8434d763553ef2d9aaaf4
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-09-07 14:38:33 -0400

    Introduction to Props in React

---
+++ React-Props/React-Props/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={styles}>
+        <h2>{this.props.count}</h2>
+      </div>
+    )
+  }
+}
+
+let count = 0
+
+setInterval(
+  function () {
+  render(<App count={count++} />, document.getElementById('root'))},
+    1000
+)
